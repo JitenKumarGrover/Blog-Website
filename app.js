d69a12b81fc65594e5287e92cef44ef3a9e460e6
Jiten Kumar Grover <39979352+JitenKumarGrover@users.noreply.github.com>
2020-06-26 03:01:54 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,79 @@
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const ejs = require("ejs");
+const _ = require('lodash');
+
+const homeStartingContent = "Too young, too simple. Sometimes, naive & stupid.";
+const aboutContent = "I am a Computer Engineering Student at Thapar Institute of Engineering And Technology Patiala, India. I am a passionate coder, and always looking for learning new technologies. I code to create new things and solve interesting problems. Loves to travel, reading books and listening music.";
+const contactContent = "I’m currently available for freelance work.";
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+mongoose.connect("mongodb+srv://admin-jiten:test123@cluster0-kjtra.mongodb.net/blogDB", {useNewUrlParser: true});
+
+const postSchema = {
+  title: String,
+  content: String
+ };
+
+ const Post = mongoose.model("Post", postSchema);
+ 
+
+app.get("/",function(req,res){
+Post.find({}, function(err, posts){
+    res.render("home", {
+      startingContent: homeStartingContent,
+      posts: posts
+      });
+  });
+});
+
+app.get("/about",function(req,res){
+  res.render("about", { startingAboutContent: aboutContent});
+  });
+
+app.get("/contact",function(req,res){
+  res.render("contact", { startingContactContent: contactContent});
+  });
+
+app.get("/compose",function(req,res){
+  res.render("compose",);
+  });
+
+app.post("/compose", function(req,res){
+  const post = new Post ({
+    title: req.body.postTitle,
+    content: req.body.postBody
+  });
+  post.save(function(err){
+    if (!err){
+      res.redirect("/");
+    }
+  });
+});
+
+app.get("/posts/:postId", function(req, res){
+
+  const requestedPostId = req.params.postId;
+  
+    Post.findOne({_id: requestedPostId}, function(err, post){
+      res.render("post", {
+        title: post.title,
+        content: post.content
+      });
+    });
+  
+  });
+
+
+app.listen(process.env.PORT || 3000, function() {
+  console.log("Server started on port 3000");
+});
